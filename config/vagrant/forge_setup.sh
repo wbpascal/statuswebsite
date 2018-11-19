9b380371927c931c96b611b8ce51e3b4f4e28fea
Pascal Wiedenbeck <wpascal@mail.uni-paderborn.de>
2018-11-19 20:29:08 +0100

    Fix forge configuration

--- config/vagrant/forge_setup.sh
+++ config/vagrant/forge_setup.sh
@@ -8,11 +8,6 @@ if [ -z `which forge` ]; then
 	curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url)/forge -o /tmp/forge
 	chmod a+x /tmp/forge
 	sudo mv /tmp/forge /usr/local/bin
-  
-	cat >/forge.yaml <<-EOL
-	registry:
-		type: local
-	EOL
 fi
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/forge_setup.sh'"

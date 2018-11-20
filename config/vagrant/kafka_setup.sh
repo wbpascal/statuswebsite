b4172f6b804b6f64aca2a9689b25a7ae53a27dde
Pascal Wiedenbeck <wpascal@mail.uni-paderborn.de>
2018-11-20 22:20:28 +0100

    Vagrant VM: Create only 1 kafka broker

--- config/vagrant/kafka_setup.sh
+++ config/vagrant/kafka_setup.sh
@@ -4,15 +4,16 @@ echo "=== Begin Vagrant Provisioning using 'config/vagrant/kafka_setup.sh'"
 
 echo "==== Creating persistant volumes for Kafka"
 sudo mkdir -p /kubernetes/kafka/broker0
-sudo mkdir -p /kubernetes/kafka/broker1
-sudo mkdir -p /kubernetes/kafka/broker2
+# sudo mkdir -p /kubernetes/kafka/broker1
+# sudo mkdir -p /kubernetes/kafka/broker2
 sudo chmod 777 -R /kubernetes/kafka
 kubectl apply -f /vagrant/config/kafka/persistant_volumes.yml
 
 echo "===== Adding Kafka to cluster"
 helm install --name kafka \
-             --set persistence.storageClass=kafka \
              --set persistence.size=1Gi \
+             --set persistence.storageClass=kafka \
+			 --set replicas=1 \
 			 incubator/kafka
 
 echo "=== End Vagrant Provisioning using 'config/vagrant/kafka_setup.sh'"

d4c63f6cc66371415ae30069190332cc21153387
Pascal Wiedenbeck <wpascal@mail.uni-paderborn.de>
2019-01-04 19:53:26 +0100

    Remove now unneeded config dir

--- config/icinga/docker/README.md
+++
@@ -1,3 +0,0 @@
-# icinga2-docker
-
-Based on [https://github.com/jjethwa/icinga2]()
--- config/vagrant/zipkin_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "=== Begin Vagrant Provisioning using 'config/vagrant/zipkin_setup.sh'"
-
-echo "=== Applying zipkin yml to cluster"
-kubectl apply -f /vagrant/config/zipkin/zipkin.yaml
-
-echo "=== End Vagrant Provisioning using 'config/vagrant/zipkin_setup.sh'"

10cb3644c556cd2937ba287d428ba7aa42753cd1
Pascal Wiedenbeck <wpascal@mail.uni-paderborn.de>
2019-01-26 21:38:47 +0100

    Finish host overview of the frontend

--- src/frontend/assets/js/service.js
+++ src/frontend/assets/js/service.js
@@ -44,27 +44,17 @@ function buildChartFromMeasurements(canvas, measurements) {
     });
 }
 
-export function createService(id, chartCanvas, statusContainer, measurements) {
-    let data = measurements.map(function (measurement) {
-        return {
-            t: moment.unix(measurement.endTime).utc().tz(moment.tz.guess()),
-            y: measurement.responseTime
-        }
-    });
-
-    return new Service(id, chartCanvas, statusContainer, data);
-}
-
+// A class representing a single service (minor or mayor)
+// Keeps track of the measurements and the status of the service and updates the chart accordingly (if given)
 export class Service {
     constructor(id, chartCanvas, statusContainer, measurements) {
         this.id = id;
         this.statusContainer = statusContainer;
         this.measurements = measurements;
-        this.chart = buildChartFromMeasurements(chartCanvas, this.measurements);
-    }
+        this.statusCallbacks = [];
 
-    get status() {
-        // if() //last element of measurements online
+        if (chartCanvas !== null) this.chart = buildChartFromMeasurements(chartCanvas, this.measurements);
+        this.updateStatus();
     }
 
     addNewMeasurement(timeTaken, timestamp) {
@@ -74,6 +64,19 @@ export class Service {
         };
         this.measurements.push(data);
         this.updateChart();
+        this.updateStatus();
+    }
+
+    addStatusCallback(fun) {
+        this.statusCallbacks.push(fun);
+    }
+
+    setStatus(status) {
+        let old_status = this.status;
+        this.status = status;
+        if (old_status !== this.status) { // If the status changed call all registered callbacks
+            this.statusCallbacks.forEach(fun => fun(status));
+        }
     }
 
     startListeningForUpdates() {
@@ -90,37 +93,111 @@ export class Service {
     }
 
     updateChart() {
+        if (this.chart === null) return; // Do not update the chart if it does not exist (minor services)
+
         this.chart.data.datasets[0].data = this.measurements;
         this.chart.update();
     }
 
     updateStatus() {
-        this.statusContainer.childNodes.forEach(child => {
+        if (this.measurements.length === 0) {
+            this.setStatus("unknown");
+        } else if (this.measurements[this.measurements.length - 1].y !== null) {
+            this.setStatus("ok");
+        } else if (this.measurements.length === 1) {
+            this.setStatus("warning");
+        } else {
+            if (this.measurements[this.measurements.length - 2] !== null) {
+                this.setStatus("warning");
+            } else {
+                this.setStatus("danger");
+            }
+        }
+
+        this.updateStatusContainer();
+    }
+
+    updateStatusContainer() {
+        Array.from(this.statusContainer.getElementsByTagName("span")).forEach(child => {
             child.classList.add("d-none");
         });
 
         switch (this.status) {
             case "ok":
-                this.statusContainer.getElementsByClassName("status-success").forEach(element => {
+                Array.from(this.statusContainer.getElementsByClassName("status-success")).forEach(element => {
                     element.classList.remove("d-none");
                 });
                 break;
             case "warning":
-                this.statusContainer.getElementsByClassName("status-warning").forEach(element => {
+                Array.from(this.statusContainer.getElementsByClassName("status-warning")).forEach(element => {
                     element.classList.remove("d-none");
                 });
                 break;
             case "danger":
-                this.statusContainer.getElementsByClassName("status-danger").forEach(element => {
+                Array.from(this.statusContainer.getElementsByClassName("status-danger")).forEach(element => {
                     element.classList.remove("d-none");
                 });
                 break;
             default:
-                this.statusContainer.getElementsByClassName("status-unknown").forEach(element => {
+                Array.from(this.statusContainer.getElementsByClassName("status-unknown")).forEach(element => {
                     element.classList.remove("d-none");
                 });
         }
     }
 }
 
+export function createService(id, chartCanvas, statusContainer, measurements) {
+    let data = measurements.map(function (measurement) {
+        return {
+            t: moment.unix(measurement.endTime).utc().tz(moment.tz.guess()),
+            y: measurement.responseTime
+        }
+    });
+
+    return new Service(id, chartCanvas, statusContainer, data);
+}
+
+// Keeps track of the overall status and updates the top status banner if needed
+export class OverallStatus {
+    constructor() {
+        this.childServices = [];
+    }
+
+    addMayorService(service) {
+        this.childServices.push(service);
+        let overallStatus = this;
+        service.addStatusCallback(function (status) {
+            if (status === "ok") overallStatus.updateStatus();
+            else overallStatus.updateStatus(status);
+        });
+    }
+
+    updateStatus(status) {
+        // If no status was defined look at the child services for more information
+        if (status === undefined) {
+            // If at least one child service is not reachable then declare the host as impacted
+            status = this.childServices.some(function (service) {
+                return service.status !== "ok";
+            }) ? "warning" : "ok";
+        }
+
+        document.getElementById("overall-status-success").classList.add("d-none");
+        document.getElementById("overall-status-warning").classList.add("d-none");
+
+        switch (status) {
+            case "ok":
+                document.getElementById("overall-status-success").classList.remove("d-none");
+                break;
+            default:
+                document.getElementById("overall-status-warning").classList.remove("d-none");
+                break;
+        }
+    }
+}
+
+export function createOverallStatus() {
+    return new OverallStatus();
+}
+
 window.createService = createService;
+window.createOverallStatus = createOverallStatus;

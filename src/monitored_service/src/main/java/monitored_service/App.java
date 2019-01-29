2673db9368649505bbb2bde728469e7448245eb3
Florian Bürger <fbuerger@mail.uni-paderborn.de>
2019-01-29 20:53:13 +0100

    dataSource aus AutoConfiguration entfernt

--- src/monitored_service/src/main/java/monitored_service/App.java
+++ src/monitored_service/src/main/java/monitored_service/App.java
@@ -5,13 +5,16 @@ package monitored_service;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://mariadb:3306/monitored_services", "root", System.getenv("MYSQL_ROOT_PASS")).load();
-        // flyway.validate();
+        //flyway.setValidateOnMigrate(false);
         flyway.migrate();
         SpringApplication.run(App.class, args);
     }

2d72d4159168b916a47bc9383465ef8d93b8fc74
Pascal Wiedenbeck <wpascal@mail.uni-paderborn.de>
2019-01-31 00:24:26 +0100

    Fix prod environment

--- src/frontend/config/prod.exs
+++ src/frontend/config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :frontend, FrontendWeb.Endpoint,
   http: [:inet6, port: 4000],
+  url: [host: "vs-status.de", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
